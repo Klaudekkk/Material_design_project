bdd46d86399c843026ae82ea01763e948a780e37
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-08-22 14:04:06 +0200

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//= require jquery
+//= require materialize/extras/nouislider
+//= require materialize-sprockets
+//= require datatables
+//= require init
+//= require activestorage
